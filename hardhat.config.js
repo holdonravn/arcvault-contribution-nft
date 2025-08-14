4280750945693800bb4ff2b233a8fb1943478449
holdonravn <tayfunmalatyali@gmail.com>
2025-08-15 00:08:26 +0300

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-require("@openzeppelin/hardhat-upgrades");
 
 const { RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    sepolia: {
-      url: RPC_URL || "",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
-    },
     mainnet: {
-      url: RPC_URL || "",
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
+    rinkeby: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY]
     }
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY || ""
+    apiKey: ETHERSCAN_API_KEY
   }
 };
